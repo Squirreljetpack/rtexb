5a22fe3a4851c773a639a8fa83da3c9c48429eaf
Linus <squirreljetpack@qq.com>
2021-10-10 05:09:57 -0400

    fix .env

--- main.py
+++ main.py
@@ -9,14 +9,13 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir,'.env'))
 
-user_agent=os.environ.get('PRAW_USER_AGENT'),
-client_id=os.environ.get('PRAW_CLIENT_ID'),
-client_secret=os.environ.get('PRAW_CLIENT_SECRET'),
-username=os.environ.get('PRAW_USERNAME'),
-password=os.environ.get('PRAW_PASSWORD'),
-url=os.environ.get('URL'),
+user_agent=os.environ.get('PRAW_USER_AGENT')
+client_id=os.environ.get('PRAW_CLIENT_ID')
+client_secret=os.environ.get('PRAW_CLIENT_SECRET')
+username=os.environ.get('PRAW_USERNAME')
+password=os.environ.get('PRAW_PASSWORD')
+url=os.environ.get('URL')
 subreddits=os.environ.get('PRAW_SUBREDDITS')
-
 
 def main():
     reddit = praw.Reddit(
@@ -29,26 +28,32 @@ def main():
 
     subreddit = reddit.subreddit(subreddits)
     for submission in subreddit.stream.submissions():
-        for comment in submission.comments:
+        submission.comments.replace_more(limit=0)
+        comments = submission.comments.list()
+        for comment in comments:
             if hasattr(comment,"body"):
                 process_comment(comment)
 
-
 def process_comment(comment):
-    if hasattr(reply,"author"):
-        if reply.author == username:
-            return
-    found = re.findall(re.compile(r"\$\$.*?\$\$"),comment.body)
-    if found:
-        for i in range(len(found)):
-            found[i] = quote(found[i].replace('\\', ''), safe='/')
-        l=[f"[{i}](http://{url}/mathjax/{s})" for i, s in enumerate(found, 1)]
-        s="I've rendered your latex: "+' '.join(l)
-        comment.reply(s)
-        time.sleep(60)
     for reply in comment.replies:
-        process_comment(reply)
-        
+        if hasattr(reply,"author"):
+            if reply.author == username:
+                break
+    else:
+        if (hasattr(comment,"author")):
+            if (comment.author != username):
+                found = re.findall(re.compile(r"\$\$.*?\$\$"),comment.body)
+                if found:
+                    for i in range(len(found)):
+                        found[i] = quote(found[i].replace('\\', ''), safe='/')
+                    l=[f"[{i}](http://{url}/mathjax/{s})" for i, s in enumerate(found, 1)]
+                    s="I've rendered your latex: "+' '.join(l)
+                    comment.reply(s)
+                    print(comment.author)
+                    print(comment.body)
+                    print(s)
+                    print("###################")
+                    time.sleep(120)
         
 
 if __name__ == "__main__":
